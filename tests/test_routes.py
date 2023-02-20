5d886cdb846d8feab94483cf0eb7e3d2494330ef
anderzzz <ohrn.anders@gmail.com>
2023-02-20 08:08:33 -0500

    Delete account method implemented

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -183,3 +183,24 @@ class TestAccountService(TestCase):
             f"{BASE_URL}/0", json=new_account
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_account(self):
+        """It should delete an Account"""
+        account = self._create_accounts(1)[0]
+        response = self.client.delete(
+            f"{BASE_URL}/{account.id}"
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_account_not_found(self):
+        """It should attemtpt to delete a non-existing Account"""
+        account = self._create_accounts(1)[0]
+        response = self.client.delete(
+            f"{BASE_URL}/0"
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
